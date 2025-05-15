f355458e1a4a3b344e46a634996d00cea11e1c3c
Your Name <you@example.com>
2025-05-15 10:24:19 +0000

    #

---
+++ build/CMakeFiles/helloworld.dir/lib.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/helloworld.dir/lib.cpp.o: /home/smuser/otus\#1/otus-1/lib.cpp \
+ /usr/include/stdc-predef.h /home/smuser/otus\#1/otus-1/lib.h \
+ /home/smuser/otus\#1/otus-1/build/version.h
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/smuser/otus#1/otus-1
+# Build directory: /home/smuser/otus#1/otus-1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/home/smuser/otus#1/otus-1/CMakeLists.txt;85;add_test;/home/smuser/otus#1/otus-1/CMakeLists.txt;0;")
